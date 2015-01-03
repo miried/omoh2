03675ff9e3eb747bb9918320bd12483cc35f6439
Michael Rieder <citizen@1984.com>
2015-01-03 01:40:25 +0100

    new cgame code (not working yet)

---
+++ code/moh_cgame/cg_consolecmds.c
@@ -0,0 +1,122 @@
+/*
+===========================================================================
+Copyright (C) 1999-2005 Id Software, Inc.
+
+This file is part of Quake III Arena source code.
+
+Quake III Arena source code is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+Quake III Arena source code is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Quake III Arena source code; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+===========================================================================
+*/
+//
+// cg_consolecmds.c -- text commands typed in at the local console, or
+// executed by a key binding
+
+#include "cg_local.h"
+
+
+/*
+=============
+CG_Viewpos_f
+
+Debugging command to print the current position
+=============
+*/
+static void CG_Viewpos_f (void) {
+	CG_Printf ("(%i %i %i) : %i\n", (int)cg.refdef.vieworg[0],
+		(int)cg.refdef.vieworg[1], (int)cg.refdef.vieworg[2], 
+		(int)cg.refdefViewAngles[YAW]);
+}
+
+
+
+typedef struct {
+	char	*cmd;
+	void	(*function)(void);
+} consoleCommand_t;
+
+static consoleCommand_t	commands[] = {
+	{ "viewpos", CG_Viewpos_f }
+};
+
+
+/*
+=================
+CG_ConsoleCommand
+
+The string has been tokenized and can be retrieved with
+Cmd_Argc() / Cmd_Argv()
+=================
+*/
+qboolean CG_ConsoleCommand( void ) {
+	const char	*cmd;
+	int		i;
+
+	cmd = CG_Argv(0);
+
+	for ( i = 0 ; i < ARRAY_LEN( commands ) ; i++ ) {
+		if ( !Q_stricmp( cmd, commands[i].cmd ) ) {
+			commands[i].function();
+			return qtrue;
+		}
+	}
+
+	return qfalse;
+}
+
+
+/*
+=================
+CG_InitConsoleCommands
+
+Let the client system know about all of our commands
+so it can perform tab completion
+=================
+*/
+void CG_InitConsoleCommands( void ) {
+	int		i;
+
+	for ( i = 0 ; i < ARRAY_LEN( commands ) ; i++ ) {
+		trap_AddCommand( commands[i].cmd );
+	}
+
+	//
+	// the game server will interpret these commands, which will be automatically
+	// forwarded to the server after they are not recognized locally
+	//
+	trap_AddCommand ("kill");
+	trap_AddCommand ("say");
+	trap_AddCommand ("say_team");
+	trap_AddCommand ("tell");
+
+	trap_AddCommand ("give");
+	trap_AddCommand ("god");
+	trap_AddCommand ("notarget");
+	trap_AddCommand ("noclip");
+	trap_AddCommand ("where");
+	trap_AddCommand ("team");
+	trap_AddCommand ("follow");
+	trap_AddCommand ("follownext");
+	trap_AddCommand ("followprev");
+	trap_AddCommand ("levelshot");
+	trap_AddCommand ("addbot");
+	trap_AddCommand ("setviewpos");
+	trap_AddCommand ("callvote"); // mohaa only adds this
+	trap_AddCommand ("vote"); // and this
+	trap_AddCommand ("callteamvote");
+	trap_AddCommand ("teamvote");
+	trap_AddCommand ("stats");
+	trap_AddCommand ("teamtask");
+	trap_AddCommand ("loaddefered");	// spelled wrong, but not changing for demo
+}
---
+++ code/moh_cgame/cg_main.c
@@ -0,0 +1,423 @@
+/*
+===========================================================================
+Copyright (C) 1999-2005 Id Software, Inc.
+
+This file is part of Quake III Arena source code.
+
+Quake III Arena source code is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+Quake III Arena source code is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Quake III Arena source code; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+===========================================================================
+*/
+//
+// cg_main.c -- initialization and primary entry point for cgame
+#include "cg_local.h"
+
+
+void CG_Init( int serverMessageNum, int serverCommandSequence, int clientNum );
+void CG_Shutdown( void );
+
+
+/*
+================
+vmMain
+
+This is the only way control passes into the module.
+This must be the very first function compiled into the .q3vm file
+================
+*/
+Q_EXPORT intptr_t vmMain( int command, int arg0, int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11  ) {
+
+	switch ( command ) {
+	case CG_INIT:
+		CG_Init( arg0, arg1, arg2 );
+		return 0;
+	case CG_SHUTDOWN:
+		CG_Shutdown();
+		return 0;
+	case CG_CONSOLE_COMMAND:
+		return CG_ConsoleCommand();
+	case CG_DRAW_ACTIVE_FRAME:
+		CG_DrawActiveFrame( arg0, arg1, arg2 );
+		return 0;
+	case CG_CROSSHAIR_PLAYER:
+		return CG_CrosshairPlayer();
+	case CG_LAST_ATTACKER:
+		return CG_LastAttacker();
+	case CG_KEY_EVENT:
+		CG_KeyEvent(arg0, arg1);
+		return 0;
+	case CG_MOUSE_EVENT:
+		CG_MouseEvent(arg0, arg1);
+		return 0;
+	case CG_EVENT_HANDLING:
+		CG_EventHandling(arg0);
+		return 0;
+	default:
+		CG_Error( "vmMain: unknown command %i", command );
+		break;
+	}
+	return -1;
+}
+
+
+cg_t				cg;
+cgs_t				cgs;
+centity_t			cg_entities[MAX_GENTITIES];
+weaponInfo_t			cg_weapons[MAX_WEAPONS];
+itemInfo_t			cg_items[MAX_ITEMS];
+
+
+vmCvar_t	cg_ignore;
+vmCvar_t	cg_wombat;
+
+typedef struct {
+	vmCvar_t	*vmCvar;
+	char		*cvarName;
+	char		*defaultString;
+	int			cvarFlags;
+} cvarTable_t;
+
+static cvarTable_t cvarTable[] = {
+	{ &cg_ignore, "cg_ignore", "0", 0 }	// used for debugging
+};
+
+static int  cvarTableSize = ARRAY_LEN( cvarTable );
+
+/*
+=================
+CG_RegisterCvars
+=================
+*/
+void CG_RegisterCvars( void ) {
+	int			i;
+	cvarTable_t	*cv;
+	char		var[MAX_TOKEN_CHARS];
+
+	for ( i = 0, cv = cvarTable ; i < cvarTableSize ; i++, cv++ ) {
+		trap_Cvar_Register( cv->vmCvar, cv->cvarName,
+			cv->defaultString, cv->cvarFlags );
+	}
+
+	// see if we are also running the server on this machine
+	trap_Cvar_VariableStringBuffer( "sv_running", var, sizeof( var ) );
+	cgs.localServer = atoi( var );
+
+
+	trap_Cvar_Register(NULL, "cg_wombat", "1", CVAR_ARCHIVE );
+}
+
+/*
+=================
+CG_UpdateCvars
+=================
+*/
+void CG_UpdateCvars( void ) {
+	int			i;
+	cvarTable_t	*cv;
+
+	for ( i = 0, cv = cvarTable ; i < cvarTableSize ; i++, cv++ ) {
+		trap_Cvar_Update( cv->vmCvar );
+	}
+
+	// check for modications here
+}
+
+int CG_CrosshairPlayer( void ) {
+	if ( cg.time > ( cg.crosshairClientTime + 1000 ) ) {
+		return -1;
+	}
+	return cg.crosshairClientNum;
+}
+
+int CG_LastAttacker( void ) {
+	if ( !cg.attackerTime ) {
+		return -1;
+	}
+	return cg.snap->ps.persistant[PERS_ATTACKER];
+}
+
+void QDECL CG_Printf( const char *msg, ... ) {
+	va_list		argptr;
+	char		text[1024];
+
+	va_start (argptr, msg);
+	Q_vsnprintf (text, sizeof(text), msg, argptr);
+	va_end (argptr);
+
+	trap_Print( text );
+}
+
+void QDECL CG_Error( const char *msg, ... ) {
+	va_list		argptr;
+	char		text[1024];
+
+	va_start (argptr, msg);
+	Q_vsnprintf (text, sizeof(text), msg, argptr);
+	va_end (argptr);
+
+	trap_Error( text );
+}
+
+void QDECL Com_Error( int level, const char *error, ... ) {
+	va_list		argptr;
+	char		text[1024];
+
+	va_start (argptr, error);
+	Q_vsnprintf (text, sizeof(text), error, argptr);
+	va_end (argptr);
+
+	trap_Error( text );
+}
+
+void QDECL Com_Printf( const char *msg, ... ) {
+	va_list		argptr;
+	char		text[1024];
+
+	va_start (argptr, msg);
+	Q_vsnprintf (text, sizeof(text), msg, argptr);
+	va_end (argptr);
+
+	trap_Print( text );
+}
+
+/*
+================
+CG_Argv
+================
+*/
+const char *CG_Argv( int arg ) {
+	static char	buffer[MAX_STRING_CHARS];
+
+	trap_Argv( arg, buffer, sizeof( buffer ) );
+
+	return buffer;
+}
+
+
+//========================================================================
+
+
+/*
+=================
+CG_RegisterSounds
+
+called during a precache command
+=================
+*/
+static void CG_RegisterSounds( void ) {
+	int		i;
+	char	items[MAX_ITEMS+1];
+	char	name[MAX_QPATH];
+	const char	*soundName;
+
+
+	//cgs.media.oneMinuteSound = trap_S_RegisterSound( "sound/feedback/1_minute.wav", qtrue );
+
+	for ( i = 1 ; i < MAX_SOUNDS ; i++ ) {
+		soundName = CG_ConfigString( CS_SOUNDS+i );
+		if ( !soundName[0] ) {
+			break;
+		}
+		if ( soundName[0] == '*' ) {
+			continue;	// custom sound
+		}
+		cgs.gameSounds[i] = trap_S_RegisterSound( soundName, qfalse );
+	}
+}
+
+
+//===================================================================================
+
+
+/*
+=================
+CG_RegisterGraphics
+
+This function may execute for a couple of minutes with a slow disk.
+=================
+*/
+static void CG_RegisterGraphics( void ) {
+	int			i;
+	char		items[MAX_ITEMS+1];
+
+	// clear any references to old media
+	memset( &cg.refdef, 0, sizeof( cg.refdef ) );
+	trap_R_ClearScene();
+
+	CG_LoadingString( cgs.mapname );
+
+	trap_R_LoadWorldMap( cgs.mapname );
+
+	// precache status bar pics
+	CG_LoadingString( "game media" );
+
+
+	cgs.media.backTileShader = trap_R_RegisterShader( "gfx/2d/backtile" );
+
+	memset( cg_items, 0, sizeof( cg_items ) );
+	memset( cg_weapons, 0, sizeof( cg_weapons ) );
+}
+
+//===========================================================================
+
+/*
+=================
+CG_ConfigString
+=================
+*/
+const char *CG_ConfigString( int index ) {
+	if ( index < 0 || index >= MAX_CONFIGSTRINGS ) {
+		CG_Error( "CG_ConfigString: bad index: %i", index );
+	}
+	return cgs.gameState.stringData + cgs.gameState.stringOffsets[ index ];
+}
+
+//==================================================================
+
+/*
+======================
+CG_StartMusic
+
+======================
+*/
+void CG_StartMusic( void ) {
+	char	*s;
+	char	parm1[MAX_QPATH], parm2[MAX_QPATH];
+
+	// start the background music
+	s = (char *)CG_ConfigString( CS_MUSIC );
+	Q_strncpyz( parm1, COM_Parse( &s ), sizeof( parm1 ) );
+	Q_strncpyz( parm2, COM_Parse( &s ), sizeof( parm2 ) );
+
+	trap_S_StartBackgroundTrack( parm1, parm2 );
+}
+
+static float CG_Cvar_Get(const char *cvar) {
+	char buff[128];
+	memset(buff, 0, sizeof(buff));
+	trap_Cvar_VariableStringBuffer(cvar, buff, sizeof(buff));
+	return atof(buff);
+}
+
+/*
+=================
+CG_Init
+
+Called after every level change or subsystem restart
+Will perform callbacks to make the loading info screen update.
+=================
+*/
+void CG_Init( int serverMessageNum, int serverCommandSequence, int clientNum ) {
+	const char	*s;
+
+	// clear everything
+	memset( &cgs, 0, sizeof( cgs ) );
+	memset( &cg, 0, sizeof( cg ) );
+	memset( cg_entities, 0, sizeof(cg_entities) );
+	memset( cg_weapons, 0, sizeof(cg_weapons) );
+	memset( cg_items, 0, sizeof(cg_items) );
+
+	cg.clientNum = clientNum;
+
+	cgs.processedSnapshotNum = serverMessageNum;
+	cgs.serverCommandSequence = serverCommandSequence;
+
+	// load a few needed things before we do any screen updates
+	cgs.media.charsetShader		= trap_R_RegisterShader( "gfx/2d/bigchars" );
+	cgs.media.whiteShader		= trap_R_RegisterShader( "white" );
+	cgs.media.charsetProp		= trap_R_RegisterShaderNoMip( "menu/art/font1_prop.tga" );
+	cgs.media.charsetPropGlow	= trap_R_RegisterShaderNoMip( "menu/art/font1_prop_glo.tga" );
+	cgs.media.charsetPropB		= trap_R_RegisterShaderNoMip( "menu/art/font2_prop.tga" );
+
+	CG_RegisterCvars();
+
+	CG_InitConsoleCommands();
+
+	cg.weaponSelect = WP_MACHINEGUN;
+
+	cgs.redflag = cgs.blueflag = -1; // For compatibily, default to unset for
+	cgs.flagStatus = -1;
+	// old servers
+
+	// get the rendering configuration from the client system
+	trap_GetGlconfig( &cgs.glconfig );
+	cgs.screenXScale = cgs.glconfig.vidWidth / 640.0;
+	cgs.screenYScale = cgs.glconfig.vidHeight / 480.0;
+
+	// get the gamestate from the client system
+	trap_GetGameState( &cgs.gameState );
+
+	// check version
+	s = CG_ConfigString( CS_GAME_VERSION );
+	if ( strcmp( s, GAME_VERSION ) ) {
+		CG_Error( "Client/Server game mismatch: %s/%s", GAME_VERSION, s );
+	}
+
+	s = CG_ConfigString( CS_LEVEL_START_TIME );
+	cgs.levelStartTime = atoi( s );
+
+	CG_ParseServerinfo();
+
+	// load the new map
+	CG_LoadingString( "collision map" );
+
+	trap_CM_LoadMap( cgs.mapname );
+
+	cg.loading = qtrue;		// force players to load instead of defer
+
+	CG_LoadingString( "sounds" );
+
+	CG_RegisterSounds();
+
+	CG_LoadingString( "graphics" );
+
+	CG_RegisterGraphics();
+
+	CG_LoadingString( "clients" );
+
+	CG_RegisterClients();		// if low on memory, some clients will be deferred
+
+	cg.loading = qfalse;	// future players will be deferred
+
+	CG_InitLocalEntities();
+
+	CG_InitMarkPolys();
+
+	// remove the last loading update
+	cg.infoScreenText[0] = 0;
+
+	// Make sure we have update values (scores)
+	CG_SetConfigValues();
+
+	CG_StartMusic();
+
+	CG_LoadingString( "" );
+
+	CG_ShaderStateChanged();
+
+	trap_S_ClearLoopingSounds( qtrue );
+}
+
+/*
+=================
+CG_Shutdown
+
+Called before every level change or subsystem restart
+=================
+*/
+void CG_Shutdown( void ) {
+	// some mods may need to do cleanup work here,
+	// like closing files or archiving session data
+}
+
